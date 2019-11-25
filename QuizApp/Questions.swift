fff6235df4254b74e1ddc9fd05e17f9dd859e9d8
Benji Mandel <benji.mandel@gmail.com>
2019-11-25 18:59:56 +0100

    fixed mistekes in QuestionsArray added variables for all the questions and the number of the current question added nextQuestionButton added start and restart button

--- QuizApp/Questions.swift
+++ QuizApp/Questions.swift
@@ -19,8 +19,8 @@ struct Question {
 var questionsArray = [
     
     Question(body: "Every Monday, Sally (drive) ..... her kids to foorball practise", answer: "drives"),
-    Question(body: "Shhhhh! Be quit! (sleep) .....", answer: "is sleeping"),
-    Question(body: "Don't forget to take your umbrella. It .....", answer: "is raining"),
+    Question(body: "Shhhhh! Be quit! John (sleep) .....", answer: "is sleeping"),
+    Question(body: "Don't forget to take your umbrella. It (rain).....", answer: "is raining"),
     Question(body: " I hate living in Seattle because it (rain, always) ..... ", answer: "always rains"),
     Question(body: "Justin (write, currenly) ..... a book about his adventures in Tibet. I hope he can find a good publisher when he is finished.", answer: "is currently writing"),
     Question(body: "Jim: Do you want to come over for a dinner tonight? \n Denise: Oh, I'm sorry I can't. I (go) ..... to a movie tonight with some friends", answer: "am going"),
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -10,26 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var questionNumber = 0
-    var questions = [String]()
+    var questionNumber = 0 {
+        didSet{
+            QuestionNumberLabel.text = "Question: \(questionNumber)/\(numberOfQuestions)"
+        }
+    }
+    var allQuestions = [Question]()
     var numberOfQuestions: Int = 0
-
+    var numberOfQuestionsCorrect: Int = 0
+    
     
     // MARK: - Oulets
     @IBOutlet weak var QuestionLabel: UILabel!
+    @IBOutlet var SubmitButtonOutlet: UIButton!
+    @IBOutlet var NextButtonOutlet: UIButton!
     @IBOutlet weak var AnswerOfQuestionTextField: UITextField!
+    @IBOutlet var QuestionNumberLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
+        
+    }
+    
+    func restart() {
+        
     }
 
+    // MARK: - IBActions
+    
     @IBAction func TextFieldEditiongDidEnd(_ sender: UITextField) {
         AnswerOfQuestionTextField.resignFirstResponder()
     }
     
+    @IBAction func StartRestartPressed(_ sender: UIButton) {
+        
+    }
     @IBAction func SubmitButtonPressed(_ sender: Any) {
+        
+    }
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        
     }
 }
 
